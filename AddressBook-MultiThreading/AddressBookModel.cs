83eb3a5521abeb82f85808bdaf4a774436035cd8
Aseem <aseemanand2@gmail.com>
2020-11-11 00:30:32 +0530

    [Aseem] Created new repo and added gitignore

---
+++ AddressBook-MultiThreading/AddressBookModel.cs
@@ -0,0 +1,26 @@
+﻿// --------------------------------------------------------------------------------------------------------------------
+// <copyright file="AddressBookModel.cs" company="Bridgelabz">
+//   Copyright © 2018 Company
+// </copyright>
+// <creator Name="Aseem Anand"/>
+// --------------------------------------------------------------------------------------------------------------------
+namespace AddressBook_MultiThreading
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Text;
+
+    public class AddressBookModel
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Address { get; set; }
+        public string City { get; set; }
+        public string State { get; set; }
+        public int Zip { get; set; }
+        public double PhoneNumber { get; set; }
+        public string Email { get; set; }
+        public string ContactType { get; set; }
+        public string AddressBookName { get; set; }
+    }
+}
